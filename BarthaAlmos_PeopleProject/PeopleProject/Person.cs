8866a0ad062c5d883ca934b1bee6cdcf56cb5a70
BarthaA <105843703+BarthaA@users.noreply.github.com>
2024-06-10 15:09:54 +0200

    Add files via upload

---
+++ BarthaAlmos_PeopleProject/PeopleProject/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleProject
+{
+    public class Person
+    {
+        private int id;
+        private string name;
+        private int age;
+        private bool isStudent;
+        private int score;
+
+        public Person(int id, string name, int age, bool isStudent, int score)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.IsStudent = isStudent;
+            this.Score = score;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set { if (value == "") { throw new ArgumentException("A név nem lehet üres!"); } else { name = value; } } }
+        public int Age { get => age; set { if (value < 1) { throw new ArgumentException("Az életkor nem lehet 1-nél kisebb!"); } else { age = value; } } }
+        public bool IsStudent { get => isStudent; set => isStudent = value; }
+        public int Score { get => score; set { if (value < 0) { throw new ArgumentException("A pontszám nem lehet 0-nál kisebb!"); } else { score = value; } } }
+    }
+}
---
+++ BarthaAlmos_PeopleProject/PeopleProject/PersonStatistics.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleProject
+{
+    public class PersonStatistics
+    {
+        private List<Person> people;
+
+        public PersonStatistics(List<Person> people)
+        {
+            this.People = people;
+        }
+
+        public List<Person> People
+        {
+            private get { return people; }
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(people));
+                }
+                else
+                {
+                    people = value;
+                }
+            }
+        }
+        
+        public double getAverageAge()
+        {
+            if(people.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return people.Average(p => p.Age);
+            }
+        }
+
+        public int getNumberOfStudents()
+        {
+            return people.Where(p => p.IsStudent).Count();
+        }
+
+        public Person getPersonWithHighestScore()
+        {
+            if(people.Count == 0)
+            {
+                throw new ArgumentException("Üres listával ez a művelet nem hajtható végre!");
+            }
+            else
+            {
+                return people.Where(p => p.Score == people.Max(p => p.Score)).First();
+            }
+        }
+
+        public double getAverageScoreOfStudents()
+        {
+            if (people.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return people.Average(p => p.Score);
+            }
+        }
+
+        public Person getOldestStudent()
+        {
+            if (people.Count == 0)
+            {
+                throw new ArgumentException("Üres listával ez a művelet nem hajtható végre!");
+            }
+            else
+            {
+                return people.Where(p => p.Age == people.Where(p => p.IsStudent).Max(p => p.Age) && p.IsStudent).First();
+            }
+        }
+
+        public bool isAnyoneFailing()
+        {
+            return people.Where(p => p.Score < 40).Count() > 0;
+        }
+    }
+}
